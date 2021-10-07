576ce43bb35373d31c007c399689c0a66a5f0397
sebastian.erik.johansson <sebastian.erik.johansson@gmail.com>
2021-10-07 21:07:24 +0200

    Add project files.

---
+++ Library/Book.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+        public Book(string title, string author, int pages)
+        {
+            Title = title;
+            Author = author;
+            Pages = pages;
+        }
+    }
+    class Textbook : Book
+    {
+        public string Subject { get; set; }
+        public int Difficulty { get; set; }
+        internal Textbook(string title, string author, int pages, string subject, int difficulty) : base(title, author, pages)
+        {
+            Subject = subject;
+            Difficulty = difficulty;
+        }
+    }
+    class Kids : Book
+    {
+        public byte ChildOrTeen { get; set; }
+        public bool Pictures { get; set; }
+        internal Kids(string title, string author, int pages, byte childOrTeen, bool pictures) : base( title, author, pages)
+        {
+            ChildOrTeen = childOrTeen;
+            Pictures = pictures;
+        }
+    }
+    class Fiction : Book
+    {
+        public string Genre { get; set; }
+        public byte RomanceOrAnthology { get; set; }
+        internal Fiction(string title, string author, int pages, string genre, byte romanceOrAnthology) : base(title, author, pages)
+        {
+            Genre = genre;
+            RomanceOrAnthology = romanceOrAnthology;
+        }
+    }
+}
---
+++ Library/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> libraryCollection = new List<Book>();
+            libraryCollection.Add(new Textbook("New Book 1", "J. Doe", 150, "Coding", 1));
+            libraryCollection.Add(new Kids("New Book 2", "K. Doe", 250, 1, false));
+            libraryCollection.Add(new Fiction("New Book 3", "L. Doe", 350, "Thriller", 0));
+
+            string menuChoice = "0";
+            while (menuChoice != "4")
+            {
+                Console.WriteLine("Choose an alternative");
+                Console.WriteLine("1. Textbooks");
+                Console.WriteLine("2. Kids");
+                Console.WriteLine("3. Fiction");
+                Console.WriteLine("4. Exit");
+                menuChoice = Console.ReadLine();
+                Console.WriteLine();
+
+                switch (menuChoice)
+                {
+                    case "1":
+                        foreach (Textbook textbook in libraryCollection)
+                        {
+                            Console.WriteLine("Title:       " + textbook.Title);
+                            Console.WriteLine("Author:      " + textbook.Author);
+                            Console.WriteLine("Pages:       " + textbook.Pages);
+                            Console.WriteLine("Subject:     " + textbook.Subject);
+                            Console.WriteLine("Difficulty:  " + textbook.Difficulty);
+                        }
+                        break;
+
+                    case "2":
+                        foreach (Kids kids in libraryCollection)
+                        {
+                            Console.WriteLine("Title:          " + kids.Title);
+                            Console.WriteLine("Author:         " + kids.Author);
+                            Console.WriteLine("Pages:          " + kids.Pages);
+                            Console.WriteLine("Child or teen:  " + kids.ChildOrTeen);
+                            Console.WriteLine("Pictures:       " + kids.Pictures);
+                        }
+                        break;
+
+                    case "3":
+                        foreach (Fiction fiction in libraryCollection)
+                        {
+                            Console.WriteLine("Title:                 " + fiction.Title);
+                            Console.WriteLine("Author:                " + fiction.Author);
+                            Console.WriteLine("Pages:                 " + fiction.Pages);
+                            Console.WriteLine("Genre:                 " + fiction.Genre);
+                            Console.WriteLine("Romance or Anthology:  " + fiction.RomanceOrAnthology);
+                        }
+                        break;
+
+                    case "4":
+                        break;
+
+                    default:
+                        Console.WriteLine("No valid choice.");
+                        break;
+                }
+            }
+        }
+    }
+}
